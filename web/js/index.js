f6f0c0f988b454640ac9ca0c42aae2dcf7cbcf88
zhiyutao <tao.reg1@gmail.com>
2021-02-22 22:49:32 -0800

    finish front end

---
+++ web/js/index.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    // let test = JSON.parse('[{"docid":0, "score":1, "url":"a"},{"docid":1, "score":2, "url":"c"},{"docid":3, "score":2, "url":"d"},{"docid":4, "score":2, "url":"v"},{"docid":5, "score":6, "url":"c"},{"docid":6, "score":2, "url":"c"}]');
+    // $.each(test, function (index, item) {
+    //     console.log(item.docid);
+    // });
+    // let formDiv = $("#result");
+    // let newTable = $(document.createElement("table"));
+    // let title = $(document.createElement("tr"));
+    // title.append($(document.createElement("td")).text("docid"));
+    // title.append($(document.createElement("td")).text("score"));
+    // title.append($(document.createElement("td")).text("url"));
+    // newTable.append(title);
+    // $.each(test, function (index, item) {
+    //     let line = $(document.createElement("tr"));
+    //     line.append($(document.createElement("td")).text(item.docid));
+    //     line.append($(document.createElement("td")).text(item.score));
+    //     line.append($(document.createElement("td")).text(item.url));
+    //     newTable.append(line);
+    // });
+    // formDiv.append(newTable);
+
+    $("#search").click(function () {
+        $("#search").prop("disabled", true);
+        let word = $("#input").val();
+        console.log(word);
+        $.ajax({
+            type: "POST",
+            url: "127.0.0.1:12345/search",
+            data: {
+                "query": $("#input").val(),
+            },
+            dataType: "json",
+        }).done(function (data) {
+            let formDiv = $("#result");
+            let newTable = $(document.createElement("table"));
+            let title = $(document.createElement("tr"));
+            title.append($(document.createElement("td")).text("docid"));
+            title.append($(document.createElement("td")).text("score"));
+            title.append($(document.createElement("td")).text("url"));
+            newTable.append(title);
+            $.each(data, function (index, item) {
+                let line = $(document.createElement("tr"));
+                line.append($(document.createElement("td")).text(item.docid));
+                line.append($(document.createElement("td")).text(item.score));
+                line.append($(document.createElement("td")).text(item.url));
+                newTable.append(line);
+            });
+            formDiv.append(newTable);
+        }).always(function () {
+            $("#search").prop("disabled", false);
+        });
+    });
+});
