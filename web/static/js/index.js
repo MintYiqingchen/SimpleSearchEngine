8273242aebf750a461683260b2b79759c330b79f
zhiyutao <tao.reg1@gmail.com>
2021-03-04 19:06:21 -0800

    beautify the front end

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -5,25 +5,35 @@ $(document).ready(function () {
     // });
     // let formDiv = $("#result");
     // let newTable = $(document.createElement("table"));
+    // newTable.addClass("table table-striped table-hover");
+    // let head = $(document.createElement("thead"));
+    // head.addClass("table-light")
     // let title = $(document.createElement("tr"));
-    // title.append($(document.createElement("td")).text("docid"));
-    // title.append($(document.createElement("td")).text("score"));
-    // title.append($(document.createElement("td")).text("url"));
-    // newTable.append(title);
+    // let th1 = $(document.createElement("th")).text("docid");
+    // let th2 = $(document.createElement("th")).text("score");
+    // let th3 = $(document.createElement("th")).text("url");
+    // th1.attr("scope", "col");
+    // th2.attr("scope", "col");
+    // th3.attr("scope", "col");
+    // title.append(th1);
+    // title.append(th2);
+    // title.append(th3);
+    // head.append(title);
+    // newTable.append(head);
+    // let body = $(document.createElement("tbody"));
     // $.each(test, function (index, item) {
     //     let line = $(document.createElement("tr"));
     //     line.append($(document.createElement("td")).text(item.docid));
     //     line.append($(document.createElement("td")).text(item.score));
     //     line.append($(document.createElement("td")).text(item.url));
-    //     newTable.append(line);
+    //     body.append(line);
     // });
+    // newTable.append(body);
     // formDiv.append(newTable);
 
     $("#search").click(function () {
         $("#search").prop("disabled", true);
         $("#result").empty();
-        // let word = $("#input").val();
-        // console.log(word);
         $.ajax({
             type: "POST",
             url: "/api/search",
@@ -33,21 +43,32 @@ $(document).ready(function () {
         }).done(function (data) {
             let formDiv = $("#result");
             let newTable = $(document.createElement("table"));
+            newTable.addClass("table table-striped table-hover table-bordered");
+            let head = $(document.createElement("thead"));
+            head.addClass("table-light")
             let title = $(document.createElement("tr"));
-            title.append($(document.createElement("td")).text("docid"));
-            title.append($(document.createElement("td")).text("score"));
-            title.append($(document.createElement("td")).text("url"));
-            newTable.append(title);
+            let th1 = $(document.createElement("th")).text("DocId");
+            let th2 = $(document.createElement("th")).text("Score");
+            let th3 = $(document.createElement("th")).text("URL");
+            th1.attr("scope", "col");
+            th2.attr("scope", "col");
+            th3.attr("scope", "col");
+            title.append(th1);
+            title.append(th2);
+            title.append(th3);
+            head.append(title);
+            newTable.append(head);
+            let body = $(document.createElement("tbody"));
             $.each(data, function (index, item) {
                 let line = $(document.createElement("tr"));
                 line.append($(document.createElement("td")).text(item.docid));
                 line.append($(document.createElement("td")).text(item.score));
-
                 let a = $(document.createElement("a"));
-                a.attr("href", item.url).text(item.url);
+                a.attr("href", item.url).attr("target", "_blank").text(item.url);
                 line.append($(document.createElement("td")).append(a));
-                newTable.append(line);
+                body.append(line);
             });
+            newTable.append(body);
             formDiv.append(newTable);
         }).always(function () {
             $("#search").prop("disabled", false);
